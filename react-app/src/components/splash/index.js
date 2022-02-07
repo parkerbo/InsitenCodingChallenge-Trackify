4795c2e04d9273872a10264e745a9db0b0d48713
Parker Bolick <bolickparker@gmail.com>
2022-02-07 01:44:02 -0500

    created and started on splash component

---
+++ react-app/src/components/splash/index.js
@@ -0,0 +1,15 @@
+import "./splash.css";
+
+const Splash = () => {
+	return (
+		<div className="splash-main">
+			<div>
+				<h1 id="splash-heading">
+					Track your target companies <br /> without the hassle.
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Splash;
